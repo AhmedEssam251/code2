f698998b692797df76d321be74d5693e0b790639
AhmedEssam251 <169101744+AhmedEssam251@users.noreply.github.com>
2024-05-07 20:26:41 +0300

    Update AC.v

--- AC.v
+++ AC.v
@@ -1,26 +1,5 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/06/2024 12:25:05 PM
-// Design Name: 
-// Module Name: AC_REG
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module AC_REG(
+module AC(
 
   output reg [15:0] Q,
   input      [15:0] Data,
